44923a8b0e63f372977f9f96ca1e708bed131530
Jesus Vazquez <jesusvzpg@gmail.com>
2024-03-17 20:02:56 +0100

    Relocate scripts, add makefile

--- scripts/cluster_create.sh
+++ scripts/cluster_create.sh
@@ -4,30 +4,33 @@ export SHELLOPTS	# propagate set to children by default
 
 IFS=$'\t\n'
 
-KIND_CONFIG_FILE='kind-config.yml'
+KIND_CONFIG_FILE='scripts/kind-config.yml'
 CLUSTER_NAME=kubecon2024-prometheus
 
-echo -n "## Checking if all Kubernetes dependencies are installed..."
+printf "INFO: Checking if all Kubernetes dependencies are installed...\n"
 command -v docker >/dev/null 2>&1 || { echo 'Please install docker (docker + https://github.com/abiosoft/colima on MacOS works well if Docker Desktop license does not work for you'; exit 1; }
 command -v go >/dev/null 2>&1 || { echo 'Please install go (https://go.dev/doc/install)'; exit 1; }
 command -v kind >/dev/null 2>&1 || { echo 'Please install kind (go install sigs.k8s.io/kind@v0.22.0)'; exit 1; }
 command -v kubectl >/dev/null 2>&1 || { echo 'Please install kubectl (https://kubernetes.io/docs/tasks/tools/#kubectl)'; exit 1; }
-echo "OK!"
+printf "All dependencies installed!\n"
 
-# Starts a local Kubernetes cluster with Kind if it doesn't exist.
-if ! kind get clusters | grep -q "^${CLUSTER_NAME}$"
+# Do nothing if cluster already exists
+if kind get clusters | grep -q "^${CLUSTER_NAME}$"
 then
-    # If you need to debug the kind creation command add the following to it
-    # --verbosity 2147483647 \
-    kind create cluster \
-      --name ${CLUSTER_NAME} \
-      --image kindest/node:v1.29.2 \
-      --config "$KIND_CONFIG_FILE" \
-      --wait 30s \
-      --retain \
-    || (docker ps && docker exec -t ${CLUSTER_NAME}-control-plane ss -tunlp && exit 1)
+  printf "WARN: Cluster %s already exists, skipping creation\n" "${CLUSTER_NAME}"
+  exit 0
 fi
 
+# If you need to debug the kind creation command add the following to it
+# --verbosity 2147483647 \
+kind create cluster \
+  --name ${CLUSTER_NAME} \
+  --image kindest/node:v1.29.2 \
+  --config "$KIND_CONFIG_FILE" \
+  --wait 30s \
+  --retain
+
 CLUSTER_API_URL=$(kubectl config view --minify -o jsonpath="{.clusters[?(@.name == \"kind-${CLUSTER_NAME}\")].cluster.server}")
-echo "# Cluster is running, kubectl should point to the new cluster at ${CLUSTER_API_URL}"
+printf "## Cluster is now running, kubectl should point to the new cluster at %s\n" "${CLUSTER_API_URL}"
 kubectl cluster-info
+exit 0
---
+++ scripts/scenario_0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eufo pipefail
+export SHELLOPTS	# propagate set to children by default
+
+IFS=$'\t\n'
+
+printf "ERROR: Not implemented\n"
+exit 1
