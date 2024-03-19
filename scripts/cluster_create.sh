0d0f6a9c9a496bd712a6c59203787c692fe1bb34
Jesus Vazquez <jesusvzpg@gmail.com>
2024-03-19 15:06:06 +0100

    comment consistency

--- scripts/cluster_create.sh
+++ scripts/cluster_create.sh
@@ -7,7 +7,7 @@ IFS=$'\t\n'
 KIND_CONFIG_FILE='scripts/kind-config.yml'
 CLUSTER_NAME=kubecon2024-prometheus
 
-printf "INFO: Checking if all Kubernetes dependencies are installed...\n"
+printf "#: Checking if all Kubernetes dependencies are installed...\n"
 command -v docker >/dev/null 2>&1 || { echo 'Please install docker (docker + https://github.com/abiosoft/colima on MacOS works well if Docker Desktop license does not work for you'; exit 1; }
 command -v go >/dev/null 2>&1 || { echo 'Please install go (https://go.dev/doc/install)'; exit 1; }
 command -v kind >/dev/null 2>&1 || { echo 'Please install kind (go install sigs.k8s.io/kind@v0.22.0)'; exit 1; }
