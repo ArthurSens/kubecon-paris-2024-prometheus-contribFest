5dde3c98018360807c85bf5db6776484ea2ad502
Arthur Silva Sens <arthursens2005@gmail.com>
2024-03-22 09:13:40 +0000

    Install KinD if not installed
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/cluster_create.sh
+++ scripts/cluster_create.sh
@@ -10,7 +10,7 @@ CLUSTER_NAME=kubecon2024-prometheus
 printf "#: Checking if all Kubernetes dependencies are installed...\n"
 command -v docker >/dev/null 2>&1 || { echo 'Please install docker (docker + https://github.com/abiosoft/colima on MacOS works well if Docker Desktop license does not work for you'; exit 1; }
 command -v go >/dev/null 2>&1 || { echo 'Please install go (https://go.dev/doc/install)'; exit 1; }
-command -v kind >/dev/null 2>&1 || { echo 'Please install kind (go install sigs.k8s.io/kind@v0.22.0) or follow https://kind.sigs.k8s.io/docs/user/quick-start/'; exit 1; }
+command -v kind >/dev/null 2>&1 || { bash ./scripts/install-kind.sh; }
 command -v kubectl >/dev/null 2>&1 || { echo 'Please install kubectl (https://kubernetes.io/docs/tasks/tools/#kubectl)'; exit 1; }
 printf "All dependencies installed!\n"
 
